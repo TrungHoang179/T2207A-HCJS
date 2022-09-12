9f4ca3222f13610edd32fc4a4d9b71c2de13a155
Msi123 <123>
2022-09-12 21:06:53 +0700

    Countdown 10'

---
+++ js/bt14.js
@@ -0,0 +1,19 @@
+var m = 10;
+var s = 0;
+function countdown1() {
+    var x = document.getElementById("second");
+    x.innerText = s;
+    s--;
+    var y = document.getElementById("minute");
+    y.innerText = m;
+
+    if(s<0){
+        s=59
+        m--
+    }
+    if(m<0){
+        clearInterval(as);
+    }
+}
+var as = setInterval(countdown1,1000);
+
